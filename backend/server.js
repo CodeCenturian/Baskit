f87a85ce7a2f1bfe83e68b7b0c6132e668e903ec
CodeCenturian <ashutoshkumariiitb@gmail.com>
2025-07-06 17:48:44 +0530

    Fix production route handling and specify Node.js engine version in package.json

--- backend/server.js
+++ backend/server.js
@@ -47,9 +47,9 @@ const PORT = process.env.PORT || 7000;
 if(process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'frontend/dist')));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
-  });
+  app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend/dist/index.html'));
+});
 }
 
 app.listen(PORT, () => {
